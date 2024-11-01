3c8895de8c55b07cc69ed9bc3ec7ed3fd863208e
Guillermo <grlara9@gmail.com>
2024-10-31 23:35:00 -0700

    implemented routing with react router dom, created Root component because header will be in all pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
+import Root from './components/Root';
 import Header from './components/Header';
+import Posts from './components/Posts';
+import Post from './components/Post';
+import NotFound from './components/NotFound';
+/*import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Routes,
+} from "react-router-dom";*/
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements, useParams } from 'react-router-dom';
 import './App.css';
 import { useState } from 'react';
 
@@ -7,26 +17,64 @@ function App() {
   const [posts, setPosts] = useState([
     {
       id: 1,
+      slug: 'Hello-React',
       title: 'Hello React',
       content: "The error you're seeing is because npm cannot locate a package.json file in your project directory (C:\Users\ROBERTO\Documents\code__projects). The package.json file is necessary for npm to know what scripts to run and which dependencies to load."
     },
     {
       id: 2,
+      slug: 'Second-posts',
       title: 'Second posts',
       content: "Simple SDK Integration: The Firebase SDK makes it easy to connect to your React app and provides simple methods for signing in, signing up, and managing user sessions."
     },
     {
       id: 3,
-      title: 'Third',
+      slug: 'Third-post',
+      title: 'Third post',
       content: "Set up a login and register form and use Firebase’s signInWithEmailAndPassword and createUserWithEmailAndPassword methods."
     },
   ])
-  
+
+  const router = createBrowserRouter( createRoutesFromElements(
+    <Route path="/" element={ <Root /> }>
+      <Route index element={<Posts posts={posts}/>} />
+      <Route path='/post/:postSlug' element={<PostWithParams posts={posts}/>}/>
+      <Route path='*' element={<NotFound />} />
+
+    </Route>
+  ))
+
+  function PostWithParams({posts}){
+    const { postSlug } = useParams();
+    const post = posts.find((post) => post.slug === postSlug);
+    return <Post post={post} />;
+  }
+
   return (
+    /*
+    <Router>
     <div className="App">
       <Header />
-     hello
+      <Routes>
+        <Route 
+          exact
+          path='/'
+          render={()=> <Posts posts={posts}/>}
+        />
+        <Route 
+          path='/post/:postSlug'
+          render={(props) => {
+            const post = post.find((post) => post.slug === props.match.params.postSlug);
+            return <Post post={post} />
+          }}
+        />
+        <Route component={NotFound}/>
+
+      </Routes>
     </div>
+    </Router>
+    */
+    <RouterProvider router={router}/>
   );
 }
 
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const Posts = ({posts}) => (
+const Post = ({post}) => (
+    <article className="post container">
+        <h1>{post.title}</h1>
+        <div>{post.content}</div>
+    </article>
     
-        <article className='posts container'>
-            <h1>Posts</h1>
-            <ul>
-             {posts.length < 0 && 
-             <li key="empty">No Posts Available!</li>
-            }  
-            {
-                posts.map(post => (
-                    <li key={post.id}><h2>{post.title}</h2></li>
-                ))
-            }
-            </ul>
-        </article>
-    )
+)
 
-
-export default Posts;
+export default Post;
